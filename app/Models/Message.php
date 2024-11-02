4a9236d5006aaf046d1084bf187362320ccb8d45
ManhAu1111 <aumanh11112004@gmail.com>
2024-11-02 13:58:23 +0700

    create-model

---
+++ app/Models/Message.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    const VIEWED = 2;
+    const UNVIEW = 1;
+    use HasFactory;
+
+    protected $table = "messages";
+
+    protected $fillable = [
+        "user_id", "friend_id", "message", "is_view"
+    ];
+}
